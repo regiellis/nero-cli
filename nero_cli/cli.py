ff0256199a8bb1e7b5173d76850be81709bddd45
Playlogic Hub (Regi Ellis) <regi@bynine.io>
2024-11-05 22:58:10 -0500

    Added usage to cli file

--- nero_cli/cli.py
+++ nero_cli/cli.py
@@ -24,6 +24,23 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+# usage: nero [-h] [--dry-run] [--download-only] [--latest] [--version VERSION] [--rollback] [--keep] [--download-dir DOWNLOAD_DIR] [--check] [--update-config]
+
+# Invoke Installer Script
+
+# options:
+#   -h, --help            show this help message and exit
+#   --dry-run             Perform a dry run without making any changes
+#   --download-only       Only download the installer without running it
+#   --latest              Check for the latest version and prompt for update
+#   --version VERSION     Specify a version to download and install
+#   --rollback            Rollback to the previous version
+#   --keep                Keep the downloaded file after installation
+#   --download-dir DOWNLOAD_DIR
+#                         Specify the directory to save downloads
+#   --check               Display current configuration and check for updates
+#   --update-config       Only update the configuration file with the current or specified version
+
 import os
 import json
 import subprocess
